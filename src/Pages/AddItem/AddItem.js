2db6fcdfa29a7afe7f51097460d14f01fa5ecea4
Nurun Nabi <nnabi.cse@gmail.com>
2022-05-01 10:56:19 +0600

    all components initially ready

---
+++ src/Pages/AddItem/AddItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AddItem.css';
+
+const AddItem = () => {
+    return (
+        <div>
+            <h2>This is Add Items</h2>
+        </div>
+    );
+};
+
+export default AddItem;
---
+++ src/Pages/ManageItems/ManageItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ManageItems.css';
+
+const ManageItems = () => {
+    return (
+        <div>
+            <h2>This is Manage Items</h2>
+        </div>
+    );
+};
+
+export default ManageItems;
